9b1bb74e6770f62f8cf791fee0c6eb18f8520934
FlavioLionelRita <flaviolrital@hotmail.com>
2022-09-09 17:18:34 +0200

    v0.0.10

--- release.sh
+++ release.sh
@@ -27,4 +27,5 @@ if [ $SOURCE_BRANCH == 'develop' ]; then
     git push origin --delete release
 else
     echo "Error: The release must be executed from the develop branch and not from the ${SOURCE_BRANCH} branch "
+    exit -1
 fi;
